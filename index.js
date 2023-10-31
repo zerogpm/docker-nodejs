54ce3ffe8a841fe954614c01fb02b4a48fe3be75
zerogpm <zerogpm@gmail.com>
2023-10-30 22:16:56 -0700

    add route

--- index.js
+++ index.js
@@ -3,13 +3,24 @@ const http = require('http');
 const PORT = 3000;
 
 const server = http.createServer((req, res) => {
-    res.writeHead(200, {
-        'Content-Type': 'applicaiton/json'
-    });
-    res.end(JSON.stringify({
-        id: 1,
-        name: 'we are the best here'
-    }));
+    if (req.url === '/') {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.end(JSON.stringify({
+            id: 1,
+            name: 'we sare the best here'
+        }));
+    }
+
+    if (req.url === '/message') {
+        res.setHeader('Content-Type', 'text/html')
+        res.write('<html>');
+        res.write('<body>');
+        res.write('<h1>we are ok</h1>');
+        res.write('</body>');
+        res.write('</html>');
+        res.end();
+    }
 });
 
 server.listen(PORT, () => {
